d8053c5df37be03f929a9dd1048bc34287e83693
Allan Rojas <allanjose91@gmail.com>
2015-11-22 06:14:41 -0600

    Summary

---
+++ send.php
@@ -0,0 +1,9 @@
+<?php
+ $firstname  = $_POST['message'];
+ $lastname  = $_POST['message'];
+ $email  = $_POST['message'];
+ $message = $_POST['message'];
+
+
+ echo ( 'I got this: ' + $firstname + ' ' + $lastname );
+?>
